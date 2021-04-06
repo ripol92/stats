0c4f1add261b84f25159bd58b003bad73060d293
Furkat Subkhonkulov <subhonkulov2@gmail.com>
2021-04-06 14:57:26 +0500

    update

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/ripol92/stats/v2
 
 go 1.16
 
-require github.com/ripol92/bank/v2 v2.0.0-20210406083100-c5cb31ad0b2c // indirect
+require github.com/ripol92/bank/v2 v2.0.0 // indirect
--- pkg/stats/stats.go
+++ pkg/stats/stats.go
@@ -8,7 +8,7 @@ func Avg(payments []types.Payment) types.Money {
 	sum := types.Money(0)
 
 	for _, payment := range payments {
-		if payment.Status != "StatusFail" {
+		if payment.Status != types.StatusFail {
 			sum += payment.Amount
 			count++
 		}
@@ -20,7 +20,7 @@ func Avg(payments []types.Payment) types.Money {
 func TotalInCategory(payments []types.Payment, category types.Category) types.Money {
 	sum := types.Money(0)
 	for _, payment := range payments {
-		if payment.Category == category && payment.Status != "StatusFail" {
+		if payment.Category == category && payment.Status != types.StatusFail {
 			sum += payment.Amount
 		}
 	}
